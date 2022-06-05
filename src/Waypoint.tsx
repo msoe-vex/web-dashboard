9c46a02389ce3a84cbcf9a6bc84baeef9c172744
hyrum66 <hyrummoses66@gmail.com>
2022-06-05 14:48:39 -0500

    Added basic attributes & getters/setters to waypoint class

--- src/Waypoint.tsx
+++ src/Waypoint.tsx
@@ -1,5 +1,17 @@
+enum Shown {HIDE, GHOST, SHOW};
+
 export class Waypoint {
     private _name: string | undefined;
+    private _actions: string[] | undefined;
+    private _followsPath: boolean = true;
+    private _shown: Shown = Shown.SHOW;
+
+    Waypoint(name: string | undefined, actions: string[] | undefined, followsPath: boolean, shown: Shown) {
+        this._name = name;
+        this._actions = actions;
+        this._followsPath = followsPath;
+        this._shown = shown;
+    }
 
     set name(name: string | undefined) {
         this._name = name;
@@ -8,5 +20,28 @@ export class Waypoint {
     get name(): string | undefined {
         return this._name;
     }
+
+    set actions(actions: string[] | undefined) {
+        this._actions = actions;
+    }
+
+    get actions(): string[] | undefined {
+        return this._actions;
+    }
+
+    set followsPath(followsPath: boolean) {
+        this._followsPath = followsPath;
+    }
     
+    get followsPath(): boolean {
+        return this._followsPath;
+    }
+
+    set shown(shown: Shown) {
+        this._shown = shown;
+    }
+
+    get shown(): Shown {
+        return this._shown;
+    }
 }
