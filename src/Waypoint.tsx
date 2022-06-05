903d9e59bd1f67197b42a3fd4f3d475b443e522a
Alex Kempen <alex.bookreader@gmail.com>
2022-06-05 11:40:48 -0500

    Fixed waypoint name setter capitalization

--- src/Waypoint.tsx
+++ src/Waypoint.tsx
@@ -1,7 +1,7 @@
 export class Waypoint {
     private _name: string | undefined;
 
-    set Name(name: string | undefined) {
+    set name(name: string | undefined) {
         this._name = name;
     }
 
