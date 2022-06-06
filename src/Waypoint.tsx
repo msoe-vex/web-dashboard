3c836772f42477e17d470fe1cce20aed3777776a
Alex Kempen <alex.bookreader@gmail.com>
2022-06-05 19:35:27 -0500

    Added additional fields to Waypoint

--- src/Waypoint.tsx
+++ src/Waypoint.tsx
@@ -11,46 +11,67 @@ export enum PathLocation {
 
 export class Waypoint {
     private _name: string;
-    private _followPath: boolean = true;
-    private _displayStyle: DisplayStyle = DisplayStyle.SHOW;
-    private _pathLocation: PathLocation = PathLocation.MIDDLE;
+    private _x: number; // meters
+    private _y: number; // meters
+    private _angle: number; // radians
+    private _heading: number; // radians
 
-    constructor(name: string, followPath: boolean, displayStyle: DisplayStyle, pathLocation: PathLocation) {
+    private _followPath: boolean;
+    private _maxVelocity: number;
+
+    private _displayStyle: DisplayStyle;
+    private _pathLocation: PathLocation;
+
+    constructor(
+        name: string,
+        x: number = 0,
+        y: number = 0,
+        angle: number = 0,
+        heading: number = 0,
+
+        followPath: boolean = false,
+        maxVelocity: number = 0,
+
+        displayStyle: DisplayStyle = DisplayStyle.SHOW,
+        pathLocation: PathLocation = PathLocation.MIDDLE
+    ) {
         this._name = name;
+        this._x = x;
+        this._y = y;
+        this._angle = angle;
+        this._heading = heading;
+
         this._followPath = followPath;
+        this._maxVelocity = maxVelocity;
+
         this._displayStyle = displayStyle;
         this._pathLocation = pathLocation;
     }
 
-    set name(name: string) {
-        this._name = name;
-    }
+    set name(name: string) { this._name = name; }
+    get name(): string { return this._name; }
 
-    get name(): string {
-        return this._name;
-    }
+    set x(x: number) { this._x = x; }
+    get x(): number { return this._x; }
 
-    set followPath(followPath: boolean) {
-        this._followPath = followPath;
-    }
+    set y(y: number) { this._y = y; }
+    get y(): number { return this._y; }
 
-    get followPath(): boolean {
-        return this._followPath;
-    }
+    set angle(angle: number) { this._angle = angle; }
+    get angle(): number { return this._angle; }
 
-    set displayStyle(displayStyle: DisplayStyle) {
-        this._displayStyle = displayStyle;
-    }
+    set heading(heading: number) { this._heading = heading; }
+    get heading(): number { return this._heading; }
 
-    get displayStyle(): DisplayStyle {
-        return this._displayStyle;
-    }
+    set maxVelocity(maxVelocity: number) { this._maxVelocity = maxVelocity; }
+    get maxVelocity(): number { return this._maxVelocity; }
 
-    set pathLocation(pathLocation: PathLocation) {
-        this._pathLocation = pathLocation;
-    }
+    set followPath(followPath: boolean) { this._followPath = followPath; }
+    get followPath(): boolean { return this._followPath; }
 
-    get pathLocation(): PathLocation {
-        return this._pathLocation;
-    }
+    set displayStyle(displayStyle: DisplayStyle) { this._displayStyle = displayStyle; }
+    get displayStyle(): DisplayStyle { return this._displayStyle; }
+
+    set pathLocation(pathLocation: PathLocation) { this._pathLocation = pathLocation; }
+    get pathLocation(): PathLocation { return this._pathLocation; }
 }
