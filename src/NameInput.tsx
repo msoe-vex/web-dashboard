644dd8a787ff5f62a04011fe729370e4d6ab9f1e
Alex Kempen <alex.bookreader@gmail.com>
2022-06-08 12:46:22 -0500

    Converted NameInput to use React Hooks. Functionality is unchanged.

--- src/NameInput.tsx
+++ src/NameInput.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Button, InputGroup } from '@blueprintjs/core';
 
 export interface NameInputProps {
@@ -6,17 +6,78 @@ export interface NameInputProps {
     updateName: (newName: string) => void;
 }
 
-interface NameInputState {
-    name: string;
-    editName: boolean;
+/**
+ * Creates a NameInput field with an edit button that can be used to return a value.
+ * The updateName function is only triggered when the name is changed to a new value. 
+ */
+export function NameInput(props: NameInputProps): JSX.Element {
+    const inputRef: any = useRef(null);
+
+    const [editName, setEditName] = useState(false);
+
+    useEffect(() => {
+        if (editName && inputRef !== null) {
+            inputRef.current.focus();
+        }
+    }, [editName]);
+
+    const onEditButtonClick: React.MouseEventHandler = () => {
+        // setState callback is used so focus is only set after inputField is enabled
+        setEditName(true);
+    };
+
+    const submitName = (): void => {
+        setEditName(false);
+        if (inputRef !== null && inputRef.current.value !== props.name) {
+            props.updateName(inputRef.current.value);
+        }
+    }
+
+    const onKeyPress: React.KeyboardEventHandler = (e: React.KeyboardEvent): void => {
+        if (e.key === 'Enter') { submitName(); }
+    }
+
+    return (
+        <InputGroup
+            inputRef={inputRef}
+            disabled={!editName}
+            defaultValue={props.name}
+            rightElement={
+                <EditButton
+                    show={!editName}
+                    onClick={onEditButtonClick}
+                />
+            }
+            onBlur={submitName}
+            onKeyPress={onKeyPress}
+        />
+    );
 }
+
+interface EditButtonProps {
+    show: boolean | undefined;
+    onClick: React.MouseEventHandler;
+}
+
+function EditButton(props: EditButtonProps): JSX.Element | null {
+    return (props.show === undefined || props.show) ? (
+        <Button
+            icon="edit"
+            minimal={true}
+            onClick={props.onClick}
+        />
+    ) : null;
+}
+
+/*
+A version of NameInput which does not use React Hooks.
 
 export class NameInput extends React.Component<NameInputProps, NameInputState> {
     private inputRef: HTMLInputElement | null = null;
 
     private constructor(props: NameInputProps) {
         super(props);
-        this.state = { name: this.props.name, editName: false };
+        this.state = { editName: false };
     }
 
     private onEditButtonClick: React.MouseEventHandler = () =>
@@ -73,3 +134,4 @@ class EditButton extends React.Component<EditButtonProps> {
         ) : (undefined);
     }
 }
+*/
