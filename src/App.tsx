cf39148edfc3ae89d25fb5bfb836c858bca480ed
Alex Kempen <alex.bookreader@gmail.com>
2022-06-25 15:28:00 -0500

    Cleaned up app imports

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
 import { store } from './Store/store';
 
 
