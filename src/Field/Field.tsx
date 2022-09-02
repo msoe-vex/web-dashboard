345dfe7de2a05830a161398e46ddeee87cf6bfe9
Alex Kempen <alex.bookreader@gmail.com>
2022-09-02 18:53:37 -0500

    Fixed internal store issue with Konva stage

--- src/Field/Field.tsx
+++ src/Field/Field.tsx
@@ -1,16 +1,17 @@
-import { Dictionary } from "@reduxjs/toolkit";
+import { Dictionary, EntityId } from "@reduxjs/toolkit";
 import { KonvaEventObject } from "konva/lib/Node";
 import React from "react";
 
 import { Layer, Line, Rect, Stage } from "react-konva";
-import finalPropsSelectorFactory from "react-redux/es/connect/selectorFactory";
+import { Provider, ReactReduxContext } from "react-redux";
+import { TypeOfExpression } from "typescript";
 import { useAppDispatch, useAppSelector } from "../Store/hooks";
 import { AppDispatch, RootState } from "../Store/store";
 import { Path, selectPathById } from "../Tree/pathsSlice";
-import { selectActiveRoutine } from "../Tree/uiSlice";
+import { selectActiveRoutine, selectHiddenWaypointIds, selectHighlightedWaypointIds, selectHoveredWaypointIds } from "../Tree/uiSlice";
 import { isControlWaypoint, selectWaypointDictionary, Waypoint, waypointMoved } from "../Tree/waypointsSlice";
 import { selectFieldHeight, selectFieldWidth } from "./fieldSlice";
-import { FieldCanvas, Units } from "./mathUtils";
+import { Transform, Units } from "./mathUtils";
 
 /**
  * We need a couple manipulators
@@ -20,11 +21,8 @@ import { FieldCanvas, Units } from "./mathUtils";
  * Robot angle (custom rotation manipulator, rendered as a dot?)
  */
 export function Field(): JSX.Element {
-    const dispatch = useAppDispatch();
-
     const [width, setWidth] = React.useState<number>(0);
     const [height, setHeight] = React.useState<number>(0);
-    // const ref = React.useRef<HTMLDivElement>();
 
     const resizeCanvas = React.useCallback(() => {
         const div = document.getElementById("field");
@@ -45,7 +43,65 @@ export function Field(): JSX.Element {
         return () => window.removeEventListener("resize", resizeCanvas);
     }, [resizeCanvas]);
 
-    const fieldCanvas = new FieldCanvas(height, width, useAppSelector(selectFieldHeight), useAppSelector(selectFieldWidth));
+    return (<div id="field">
+        {/* Consumer is a component which takes a function as a child */}
+        <ReactReduxContext.Consumer>
+            {
+                ({ store }) => {
+                    // Avoid using useAppSelector to prevent issues with react hooks
+                    const fieldHeight = selectFieldHeight(store.getState());
+                    const fieldWidth = selectFieldWidth(store.getState());
+                    const fieldTransform = computeFieldTransform(height, width, fieldHeight, fieldWidth);
+
+                    return (
+                        <Stage width={width} height={height}>
+                            {/* Make store available again inside stage */}
+                            <Provider store={store}>
+                                <Layer {...fieldTransform}>
+                                    <Rect
+                                        x={0.5 * Units.INCH}
+                                        y={0.5 * Units.INCH}
+                                        width={fieldWidth - 1 * Units.INCH}
+                                        height={fieldHeight - 1 * Units.INCH}
+                                        strokeWidth={1 * Units.INCH}
+                                        stroke="black"
+                                        fill="grey"
+                                    />
+                                </Layer>
+                                <Layer {...fieldTransform}>
+                                    <RobotElements />
+                                </Layer>
+                            </Provider>
+                        </Stage>
+                    );
+                }
+            }
+        </ReactReduxContext.Consumer>
+    </div >);
+};
+
+function computeFieldTransform(canvasHeight: number, canvasWidth: number, fieldHeight: number, fieldWidth: number): Transform {
+    const heightToWidth = fieldWidth / fieldHeight;
+    const widthToHeight = fieldHeight / fieldWidth;
+
+    const height = Math.min(
+        canvasHeight,
+        canvasWidth * widthToHeight
+    );
+    const width = height * heightToWidth;
+    const PIXEL = height / fieldHeight;
+
+    const xShift = (canvasWidth - width) / 2;
+    const yShift = (canvasHeight - height) / 2 + height;
+    return { x: xShift, y: yShift, scaleX: PIXEL, scaleY: -PIXEL };
+
+}
+
+interface RobotElementsProps {
+}
+
+export function RobotElements(props: RobotElementsProps): JSX.Element {
+    const dispatch = useAppDispatch();
 
     const paths = useAppSelector((state: RootState) => {
         const activeRoutine = selectActiveRoutine(state);
@@ -59,52 +115,52 @@ export function Field(): JSX.Element {
     });
     const waypointDict = useAppSelector(selectWaypointDictionary);
 
-    const robotElements = getRobotRectangles(dispatch, paths, waypointDict, fieldCanvas);
-    const pathSplines = getPathSplines(dispatch, paths, waypointDict);
+    const hiddenIds = useAppSelector(selectHiddenWaypointIds);
+    const selectedIds = useAppSelector(selectHighlightedWaypointIds);
+    const hoveredIds = useAppSelector(selectHoveredWaypointIds);
 
-    return (<div id="field">
-        <Stage width={width} height={height}>
-            {/* <Layer>
-                <Rect width={width} height={height} fill="red" />
-            </Layer> */}
-            <Layer {...fieldCanvas.fieldTransform}>
-                <Rect
-                    x={0.5 * Units.INCH}
-                    y={0.5 * Units.INCH}
-                    width={useAppSelector(selectFieldWidth) - 1 * Units.INCH}
-                    height={useAppSelector(selectFieldHeight) - 1 * Units.INCH}
-                    strokeWidth={1 * Units.INCH}
-                    stroke="black"
-                    fill="grey"
-                />
-            </Layer>
-            <Layer {...fieldCanvas.fieldTransform}>
-                {robotElements}
-                {pathSplines}
-            </Layer>
-            {/* <Layer {...fieldCanvas.fieldTransform}>
-            </Layer> */}
-        </Stage>
-    </div >);
-};
+    const elements = getRobotElements(
+        dispatch,
+        paths,
+        waypointDict,
+        hiddenIds,
+        selectedIds,
+        hoveredIds
+    );
 
-const getRobotRectangles = (dispatch: AppDispatch, paths: Path[], waypointDict: Dictionary<Waypoint>, fieldCanvas: FieldCanvas): JSX.Element[] => {
+    return (<>
+        {elements}
+    </>);
+}
+
+const getRobotElements = (
+    dispatch: AppDispatch,
+    paths: Path[],
+    waypointDict: Dictionary<Waypoint>,
+    hiddenIds: EntityId[],
+    selectedIds: EntityId[],
+    hoveredIds: EntityId[]
+): JSX.Element[] => {
     return paths.flatMap(path => {
-        return path.waypointIds.map(waypointId => {
-            const waypoint = waypointDict[waypointId];
-            if (!waypoint) { throw Error("Field expected valid waypoint."); }
+        let elements: JSX.Element[] = [];
+
+        for (let i = 0; i < path.waypointIds.length; i++) {
+            const waypoint = waypointDict[path.waypointIds[i]];
+            if (!waypoint) { throw Error("Path spline expected valid waypoint."); }
+
+            if (hiddenIds.includes(waypoint.id)) { continue; }
 
             const onWaypointDrag = (e: KonvaEventObject<MouseEvent>) => {
                 dispatch(waypointMoved({
-                    id: waypointId,
+                    id: waypoint.id,
                     x: e.target.x() + 9 * Units.INCH,
                     y: e.target.y() + 9 * Units.INCH
                 }));
             };
 
             if (isControlWaypoint(waypoint)) {
-                return (<Rect
-                    key={waypoint.id}
+                elements.push(<Rect
+                    key={"Robot" + waypoint.id}
                     x={waypoint.x - 9 * Units.INCH}
                     y={waypoint.y - 9 * Units.INCH}
                     width={18 * Units.INCH}
@@ -112,35 +168,29 @@ const getRobotRectangles = (dispatch: AppDispatch, paths: Path[], waypointDict: 
                     rotation={waypoint.robotAngle ? waypoint.robotAngle / Units.DEGREE : 0}
                     strokeWidth={0.5 * Units.INCH}
                     stroke="black"
-                    draggable={true}
+                    // shadowEnabled={selectedIds.includes(waypoint.id)}
+                    draggable={selectedIds.includes(waypoint.id)}
                     onDragMove={onWaypointDrag}
                     onDragEnd={onWaypointDrag}
                 />);
             }
-            else {
-                return (<Rect />);
+
+            if (i !== 0) {
+                const prev = waypointDict[path.waypointIds[i - 1]];
+                if (!prev) { throw Error("Path spline expected valid waypoint."); }
+                if (!isControlWaypoint(prev) || !isControlWaypoint(waypoint)) {
+                    throw Error("Follower waypoints are not supported by getPathSplines.");
+                }
+
+                elements.push(<Line
+                    key={"spline" + waypoint.id}
+                    points={[prev.x, prev.y, waypoint.x, waypoint.y]}
+                    strokeWidth={0.5 * Units.INCH}
+                    stroke="black"
+                />);
             }
-        });
-    });
-};
-
-
-const getPathSplines = (dispatch: AppDispatch, paths: Path[], waypointDict: Dictionary<Waypoint>): JSX.Element[] => {
-    return paths.flatMap(path => {
-        let splines: JSX.Element[] = [];
-        for (let i = 1; i < path.waypointIds.length; i++) {
-            const prev = waypointDict[path.waypointIds[i - 1]];
-            const waypoint = waypointDict[path.waypointIds[i]];
-            if (!prev || !waypoint) { throw Error("Path spline expected valid waypoint."); }
-            if (!isControlWaypoint(prev) || !isControlWaypoint(waypoint)) { throw Error("Follower waypoints are not supported by getPathSplines."); }
-
-            splines.push(<Line
-                key={waypoint.id}
-                points={[prev.x, prev.y, waypoint.x, waypoint.y]}
-                strokeWidth={0.5 * Units.INCH}
-                stroke="black"
-            />);
         }
-        return splines;
+
+        return elements;
     });
 };
