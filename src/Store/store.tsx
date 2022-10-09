0d8ad6b44d93dca98c055ee21eb02b262a4a6b86
ViviVoid <andydao2020@gmail.com>
2022-10-09 18:14:05 -0500

    Added Store Type

--- src/Store/store.tsx
+++ src/Store/store.tsx
@@ -93,3 +93,4 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, AnyAction>;
+export type Store = typeof store;
