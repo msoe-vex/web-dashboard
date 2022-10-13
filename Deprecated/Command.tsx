c2ea96b99851fe82f6049ad435f8b20873e51066
Alex Kempen <alex.bookreader@gmail.com>
2022-10-13 18:57:38 -0500

    Deleted deprecated folder

--- Deprecated/Command.tsx
+++
@@ -1,7 +0,0 @@
-/**
- * An interface defining a simple Command which can be used to manipulate elements in the Application.
- */
-export interface Command {
-    undo(): boolean;
-    execute(): boolean;
-}
--- Deprecated/DisplayStyle.tsx
+++
@@ -1,1 +0,0 @@
-export enum DisplayStyle { HIDE, GHOST, SHOW };
